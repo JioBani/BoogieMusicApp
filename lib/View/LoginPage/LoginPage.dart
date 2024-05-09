5df30766f81de2763ea9c64bd12d06a36f766704
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-09 17:26:23 +0900

    키보드 overflow , 로그인 페이지에서 회원가입 페이지로 이동

--- lib/View/LoginPage/LoginPage.dart
+++ lib/View/LoginPage/LoginPage.dart
@@ -3,6 +3,7 @@ import 'package:database_project/Common/LoadingDialog.dart';
 import 'package:database_project/Service/LoginService.dart';
 import 'package:database_project/Style/ShadowPalette.dart';
 import 'package:database_project/View/Common/StyledInputFieldWidget.dart';
+import 'package:database_project/View/RegistrationPage/RegistrationPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -18,129 +19,133 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 5.w , top: 10.h),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  onPressed: (){
-                    Get.back();
-                  },
-                  icon: Icon(
-                    Icons.arrow_back_ios_rounded,
-                    size: 30.sp,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 90.h,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "로그인",
-                  style: TextStyle(
-                      fontSize: 25.sp,
-                      fontWeight: FontWeight.w700
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 40.h,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "로그인 해서 부기뮤직의 다양한 \n서비스를 이용해 보세요.",
-                  style: TextStyle(
-                      fontSize: 15.sp,
-                      fontWeight: FontWeight.w700
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 30.h,),
-            StyledInputFieldWidget(content: "아이디", controller: idController,),
-            SizedBox(height: 20.h,),
-            StyledInputFieldWidget(content: "비밀번호", controller: passwordController, isPassword: true,),
-            SizedBox(height: 20.h,),
-            Padding(
-              padding:EdgeInsets.only(left: 20.w , right: 20.w),
-              child: InkWell(
-                onTap: () async {
-                  final (result , isClosed) = await LoadingDialog.showLoadingDialogWithFuture(
-                      context,
-                      LoginService.instance.login(idController.text, passwordController.text)
-                  );
-
-                  if(!result.isSuccess){
-                    Fluttertoast.showToast(msg: "로그인에 실패했습니다. : ${result.exception}");
-                  }
-                  else{
-                    Fluttertoast.showToast(msg: "로그인에 성공했습니다.");
-                    if(context.mounted){
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 5.w , top: 10.h),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    onPressed: (){
                       Get.back();
-                    }
-                  }
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 50.h,
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(5.r),
-                      boxShadow: [
-                        ShadowPalette.defaultShadowLight
-                      ]
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: 30.sp,
+                    ),
                   ),
-                  child: Center(
-                    child: Text(
-                      "로그인하기",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700
+                ),
+              ),
+              SizedBox(height: 90.h,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "로그인",
+                    style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.w700
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40.h,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "로그인 해서 부기뮤직의 다양한 \n서비스를 이용해 보세요.",
+                    style: TextStyle(
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w700
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.h,),
+              StyledInputFieldWidget(content: "아이디", controller: idController,),
+              SizedBox(height: 20.h,),
+              StyledInputFieldWidget(content: "비밀번호", controller: passwordController, isPassword: true,),
+              SizedBox(height: 20.h,),
+              Padding(
+                padding:EdgeInsets.only(left: 20.w , right: 20.w),
+                child: InkWell(
+                  onTap: () async {
+                    final (result , isClosed) = await LoadingDialog.showLoadingDialogWithFuture(
+                        context,
+                        LoginService.instance.login(idController.text, passwordController.text)
+                    );
+        
+                    if(!result.isSuccess){
+                      Fluttertoast.showToast(msg: "로그인에 실패했습니다. : ${result.exception}");
+                    }
+                    else{
+                      Fluttertoast.showToast(msg: "로그인에 성공했습니다.");
+                      if(context.mounted){
+                        Get.back();
+                      }
+                    }
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5.r),
+                        boxShadow: [
+                          ShadowPalette.defaultShadowLight
+                        ]
+                    ),
+                    child: Center(
+                      child: Text(
+                        "로그인하기",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w700
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10.h,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
-              child: Row(
-                children: [
-                  Text(
-                    "아이디가 없으신가요?",
-                    style: TextStyle(
-                        fontSize: 15.sp,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.grey
+              SizedBox(height: 10.h,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
+                child: Row(
+                  children: [
+                    Text(
+                      "아이디가 없으신가요?",
+                      style: TextStyle(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.grey
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 10.w,),
-                  TextButton(
-                      onPressed: (){},
-                      child: Text(
-                        "회원가입",
-                        style: TextStyle(
-                            fontSize: 15.sp,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black
-                        ),
-                      )
-                  )
-                ],
+                    SizedBox(width: 10.w,),
+                    TextButton(
+                        onPressed: (){
+                          Get.to(RegistrationPage());
+                        },
+                        child: Text(
+                          "회원가입",
+                          style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black
+                          ),
+                        )
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
+            ],
+          )
+        ),
       ),
     );
   }
--- lib/View/RegistrationPage/RegistrationPage.dart
+++ lib/View/RegistrationPage/RegistrationPage.dart
@@ -69,147 +69,149 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 5.w , top: 10.h),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  onPressed: (){
-                    Get.back();
-                  },
-                  icon: Icon(
-                    Icons.arrow_back_ios_rounded,
-                    size: 30.sp,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 50.h,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "회원가입",
-                  style: TextStyle(
-                      fontSize: 25.sp,
-                      fontWeight: FontWeight.w700
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 30.h,),
-            StyledInputFieldWidget(content: "아이디",controller: idController,),
-            SizedBox(height: 20.h,),
-            StyledInputFieldWidget(content: "이름",controller:nameController ,),
-            SizedBox(height: 20.h,),
-            StyledInputFieldWidget(content: "비빌번호",controller: passwordController,isPassword: true,),
-            SizedBox(height: 20.h,),
-            StyledInputFieldWidget(content: "비밀번호 확인",controller: passwordReController, isPassword: true,),
-            SizedBox(height: 20.h,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
-              child: Row(
-                children: [
-                  Text(
-                    "아이디가 있으신가요?",
-                    style: TextStyle(
-                        fontSize: 15.sp,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.grey
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 5.w , top: 10.h),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    onPressed: (){
+                      Get.back();
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: 30.sp,
                     ),
                   ),
-                  SizedBox(width: 10.w,),
-                  TextButton(
-                      onPressed: (){},
-                      child: Text(
-                        "로그인",
-                        style: TextStyle(
-                            fontSize: 15.sp,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black
-                        ),
-                      )
-                  )
-                ],
+                ),
               ),
-            ),
-            SizedBox(height: 20.h,),
-            Padding(
-              padding:EdgeInsets.only(left: 20.w , right: 20.w),
-              child: InkWell(
-                onTap: () async {
-                  User user = User(
-                      id: idController.text,
-                      name: nameController.text,
-                      role: ""
-                  );
-
-                  CreateUserDto createUserDto = CreateUserDto(
-                      id: idController.text,
-                      name: nameController.text,
-                      password: passwordController.text
-                  );
-
-
-                  var (result , msg) = createUserDto.checkValidate();
-
-                  if(!result){
-                    Fluttertoast.showToast(msg: msg);
-                    return;
-                  }
-
-                  if(passwordController.text != passwordReController.text){
-                    Fluttertoast.showToast(msg: "비밀번호 확인이 다릅니다.");
-                    StaticLogger.logger.i("${passwordController.text} ,  ${passwordReController.text}");
-                    return;
-                  }
-
-                  final (signInRes , isClosed) = await LoadingDialog.showLoadingDialogWithFuture(
-                      context,
-                      LoginService.instance.signIn(createUserDto)
-                  );
-
-                  if(!signInRes.isSuccess){
-                    buildSuccessDialog(context, "${signInRes.exception}");
-                  }
-                  else{
-                    buildSuccessDialog(context, "회원가입에 성공했습니다.").then(
-                        (value) => {
-                          if(context.mounted){
-                            Navigator.of(context).pop()
-                          }
-                        }
-                    );
-                  }
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 50.h,
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(5.r),
-                      boxShadow: [
-                        ShadowPalette.defaultShadowLight
-                      ]
+              SizedBox(height: 50.h,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "회원가입",
+                    style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.w700
+                    ),
                   ),
-                  child: Center(
-                    child: Text(
-                      "회원가입하기",
+                ),
+              ),
+              SizedBox(height: 30.h,),
+              StyledInputFieldWidget(content: "아이디",controller: idController,),
+              SizedBox(height: 20.h,),
+              StyledInputFieldWidget(content: "이름",controller:nameController ,),
+              SizedBox(height: 20.h,),
+              StyledInputFieldWidget(content: "비빌번호",controller: passwordController,isPassword: true,),
+              SizedBox(height: 20.h,),
+              StyledInputFieldWidget(content: "비밀번호 확인",controller: passwordReController, isPassword: true,),
+              SizedBox(height: 20.h,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.w, 0 , 0, 0),
+                child: Row(
+                  children: [
+                    Text(
+                      "아이디가 있으신가요?",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.grey
+                      ),
+                    ),
+                    SizedBox(width: 10.w,),
+                    TextButton(
+                        onPressed: (){},
+                        child: Text(
+                          "로그인",
+                          style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h,),
+              Padding(
+                padding:EdgeInsets.only(left: 20.w , right: 20.w),
+                child: InkWell(
+                  onTap: () async {
+                    User user = User(
+                        id: idController.text,
+                        name: nameController.text,
+                        role: ""
+                    );
+        
+                    CreateUserDto createUserDto = CreateUserDto(
+                        id: idController.text,
+                        name: nameController.text,
+                        password: passwordController.text
+                    );
+        
+        
+                    var (result , msg) = createUserDto.checkValidate();
+        
+                    if(!result){
+                      Fluttertoast.showToast(msg: msg);
+                      return;
+                    }
+        
+                    if(passwordController.text != passwordReController.text){
+                      Fluttertoast.showToast(msg: "비밀번호 확인이 다릅니다.");
+                      StaticLogger.logger.i("${passwordController.text} ,  ${passwordReController.text}");
+                      return;
+                    }
+        
+                    final (signInRes , isClosed) = await LoadingDialog.showLoadingDialogWithFuture(
+                        context,
+                        LoginService.instance.signIn(createUserDto)
+                    );
+        
+                    if(!signInRes.isSuccess){
+                      buildSuccessDialog(context, "${signInRes.exception}");
+                    }
+                    else{
+                      buildSuccessDialog(context, "회원가입에 성공했습니다.").then(
+                          (value) => {
+                            if(context.mounted){
+                              Navigator.of(context).pop()
+                            }
+                          }
+                      );
+                    }
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5.r),
+                        boxShadow: [
+                          ShadowPalette.defaultShadowLight
+                        ]
+                    ),
+                    child: Center(
+                      child: Text(
+                        "회원가입하기",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w700
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
